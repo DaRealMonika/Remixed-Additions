9f396c7b39e11d24251a5d3441ac1ee91a484aa7
Monika <39466656+DaRealMonika@users.noreply.github.com>
2020-04-14 20:49:22 -0400

    Add files via upload

---
+++ scripts/actors/Bosses/BoneDragon.lua
@@ -0,0 +1,25 @@
+--
+-- User: mike
+-- Date: 06.11.2017
+-- Time: 23:57
+-- This file is part of Remixed Pixel Dungeon.
+--
+
+local RPD = require "scripts/lib/commonClasses"
+local actor = require "scripts/lib/actor"
+
+return actor.init({
+    act = function()
+        local levelSize = RPD.Dungeon.level:getLength()
+        local cell = math.random(levelSize)-1
+        if not RPD.Dungeon.level.solid[cell] then
+            RPD.placeBlob( RPD.Blobs.Fire, cell, 10)
+        end
+        return true
+    end,
+    actionTime = function()
+        return 1
+    end,
+    activate = function()
+    end
+})
